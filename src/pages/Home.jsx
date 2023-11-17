ac659eca3b917074122b5556f08dba84e7d43cdc
jvsand <jave.shunya.623@gmail.com>
2023-11-17 23:03:22 +0900

    ページネーションの作成完了

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,18 @@
 import React, { useState, useEffect } from "react";
-// import { Link } from 'react-router-dom';
 import axios from "axios";
 import SignOut from "./SignOut";
 import { useCookies } from "react-cookie";
 import "./home.scss";
+import Pagination from './Pagination';
 const url = "https://railway.bookreview.techtrain.dev";
 
 export function Home() {
   const [, setErrorMessage] = useState("");
   const [books, setBooks] = useState([]);
   const [cookies] = useCookies();
+
+  const [currentPage, setCurrentPage] = useState(0); 
+  const booksPerPage = 3; // 1ページあたりのアイテム数
 
   // ページ読み込みでの一覧取得
   useEffect(() => {
@@ -28,21 +31,36 @@ export function Home() {
       });
   }, [cookies.token]);
 
+  const handlePageClick = (data) => {
+    const selectedPage = data.selected;
+    setCurrentPage(selectedPage);
+  };
+  const offset = currentPage * booksPerPage;
+  const currentPageBooks = books.slice(offset, offset + booksPerPage);
+
+
   return (
     <div className="home">
       <h1 className="home__title">書籍一覧</h1>
       <div className="home__book-list">
-        <ul className="book-list__items">
-          {books.map((book) => (
-            <li className="book-item" key={book.id}>
-              <span className="book-item__title">title:{book.title}</span>
+      {/* ここに書籍一覧を表示するロジックを追加 */}
+      <ul className="book-list__items">
+        {currentPageBooks.map((book) => (
+          <li className="book-item" key={book.id}>{book.title}
+          <span className="book-item__title">title:{book.title}</span>
               <p className="book-item__review">review: {book.review}</p>
               <p className="book-item__detail">detail: {book.detail}</p>
               <p className="book-item__url">url: {book.url}</p>
-            </li>
-          ))}
-        </ul>
-      </div>
+          </li>
+        ))}
+      </ul>
+
+      {/* react-paginate コンポーネントの設置 */}
+      <Pagination
+          pageCount={Math.ceil(books.length / booksPerPage)}
+          handlePageClick={handlePageClick}
+        />
+    </div>
       <SignOut />
     </div>
   );
---
+++ src/pages/Pagination.jsx
@@ -0,0 +1,22 @@
+
+import React from 'react';
+import ReactPaginate from 'react-paginate';
+import "./home.scss";
+
+const Pagination = ({ pageCount, handlePageClick }) => {
+  return (
+    <ReactPaginate
+      previousLabel={'<'}
+      nextLabel={'>'}
+      breakLabel={'...'}
+      pageCount={pageCount}
+      marginPagesDisplayed={2}
+      pageRangeDisplayed={5}
+      onPageChange={handlePageClick}
+      containerClassName={'pagination'}
+      activeClassName={'active'}
+    />
+  );
+};
+
+export default Pagination;
